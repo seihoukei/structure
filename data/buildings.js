4a534037efec67fad7d07baa98301b15994a6087
seihoukei <31934643+seihoukei@users.noreply.github.com>
2018-03-21 09:05:30 +0200

    Add files via upload

--- data/buildings.js
+++ data/buildings.js
@@ -28,7 +28,7 @@ const BUILDINGS = {//function context == point
 			return this.depth * 1000000
 		},
 		info() {
-			return "Production: " + displayNumber(this.depth * this.map.level ** 2 / 1e5 * ((this.level || 0) + 1) * ((this.level || 0) + 1) * (game.skills.magicBoost1 && (this.distance < this.map.ownedRadius)?(this.map.ownedRadius - this.distance + 1):1))
+			return "Production: " + displayNumber(this.depth * this.map.level ** 2 / 1e5 * ((this.level || 0) + 1) * (game.skills.magicBoost1 && (this.distance < this.map.ownedRadius)?(this.map.ownedRadius - this.distance + 1):1))
 		},
 		build() {
 			game.production.science += this.depth * this.map.level ** 2 / 1e5 * ((this.level || 0) + 1) * (game.skills.magicBoost1 && (this.distance < this.map.ownedRadius)?(this.map.ownedRadius - this.distance + 1):1)

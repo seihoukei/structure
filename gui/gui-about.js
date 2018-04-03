6a644501a798cb39c058c2d28e46d71a47fdd67f
seihoukei <31934643+seihoukei@users.noreply.github.com>
2018-04-03 13:21:51 +0200

    Add files via upload

--- gui/gui-about.js
+++ gui/gui-about.js
@@ -5,8 +5,8 @@ const AboutTab = Template({
 		this.dvDisplay = createElement("div", "about "+(this.className || ""), this.parent)
 		this.dvGameTitle = createElement("div", "title", this.dvDisplay, "Structure")
 		this.dvSubtitle = createElement("div", "subtitle", this.dvDisplay, "The incremental game")
-		//this.dvVersion = createElement("div", "version", this.dvDisplay, "v0.0.3 -- 26 March 2018")
-		this.dvVersion = createElement("div", "version", this.dvDisplay, "v0.0.2 -- 25 March 2018")
+		this.dvVersion = createElement("div", "version", this.dvDisplay, "v0.0.3 -- 03 April 2018")
+		//this.dvVersion = createElement("div", "version", this.dvDisplay, "v0.0.2 -- 25 March 2018")
 		this.dvAuthor = createElement("div", "author", this.dvDisplay, "by seihoukei")
 		this.dvLinks = createElement("div", "links", this.dvDisplay)
 		this.dvDiscord = createElement("div", "link", this.dvLinks, "TBA: Discord link")
@@ -18,11 +18,14 @@ const AboutTab = Template({
 		this.dvFallback = createElement("div", "credits", this.dvDisplay, "Fallback unicode font : 'Symbola' by George Douros")
 		this.dvChangelog = createElement("div", "changelog", this.dvDisplay, `Changelog
 		
-		${/*		-- 0.0.3 -- 26 March 2018 --
-		- Story is up!
-		*/""}
+		-- 0.0.3 -- 26 March 2018 --
+		- UI improvements
+		- Low load mode
+		- Improved offline progress calculation
+		- Content up to Map 27 (maps up to 29 are safe)
+		
 		-- 0.0.2 -- 24 March 2018 --
-		- Content up to GameMap level 20
+		- Content up to Map level 20
 		- Rough ETA display
 		- Proper dark theme
 		

7f25ebc52eaebdae03797b3625d0e4d2036af523
seihoukei <31934643+seihoukei@users.noreply.github.com>
2018-04-17 21:40:49 +0200

    Add files via upload

--- gui/gui-stardust.js
+++ gui/gui-stardust.js
@@ -20,7 +20,7 @@ const StardustTab = Template({
 				className : "stardust",
 				sliderClass : "bg-"+x,
 				onSet : () => {
-					game.stardust[x] = Math.round(game.stardust(x))
+					game.stardust[x] = Math.round(game.stardust[x])
 					const otherTotal = POINT_TYPES.slice(1).reduce((v, y) => y != x?v + game.stardust[y]:v, 0)
 					let otherDust = game.resources.stardust - game.stardust[x]
 					const scale = otherDust / otherTotal

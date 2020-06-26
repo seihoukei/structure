04d74a1387ddc5261aed51a226f886c9f17fffc3
seihoukei <31934643+seihoukei@users.noreply.github.com>
2020-06-26 19:28:07 +0300

    Update slider.js

--- engine/slider.js
+++ engine/slider.js
@@ -883,7 +883,7 @@ const sliderHandler = {
 				if (this.artifacts.targetOrb && this.target && (this.target.type == n + 1)) this.real.multi[x] *= 5
 				if (this.artifacts.masterOrb && this.target && (this.target.type == (n + 1) % 4 + 3)) this.real.multi[x] *= 3
 				if (ARTIFACTS.superTargetOrb.equipped && ARTIFACTS.superTargetOrb.equipped.target && (ARTIFACTS.superTargetOrb.equipped.target.type == n + 1)) this.real.multi[x] *= 3
-				if (ARTIFACTS.superMasterOrb.equipped && ARTIFACTS.superMasterOrb.equipped.target && (ARTIFACTS.superMasterOrb.equipped.target.type == (n + 1) % 4 + 3)) this.real.multi[x] *= 2
+				if (ARTIFACTS.superMasterOrb.equipped && ARTIFACTS.superMasterOrb.equipped.target && (ARTIFACTS.superMasterOrb.equipped.target.type == (n + 3) % 4 + 3)) this.real.multi[x] *= 2
 			}
 			
 			if (target && target.index == 0 && game.world.stats[x+"Boost"]) real.multi[x] *= (game.world.stats[x+"Boost"])
